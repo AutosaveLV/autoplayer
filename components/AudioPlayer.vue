6d512963b37884edcc9889d1cdd2019b38ffea6a
Filips Alpe <filips.alpe@gmail.com>
2023-12-14 16:02:49 +0200

    Display track name

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -27,18 +27,22 @@ function getVolumeColor(volume: number) {
 
 const songs = [
   {
+    name: "Track 1",
     publicPath: "audio/Track_1.mp3",
     bpm: 117,
   },
   {
+    name: "Track 2",
     publicPath: "audio/Track_2.mp3",
     bpm: 120,
   },
   {
+    name: "Track 3",
     publicPath: "audio/Track_3.mp3",
     bpm: 120,
   },
   {
+    name: "Track 4",
     publicPath: "audio/Track_4.mp3",
     bpm: 102,
   },
@@ -160,7 +164,8 @@ export default {
         :strokeWidth="strokeWidth * 0.8"
       />
     </div>
-    <div class="flex justify-center">
+    <div class="flex flex-col justify-center items-center">
+      <p>{{ currentSong.name }}</p>
       <audio ref="audio" :src="currentSong.publicPath"></audio>
       <Button
         @click="togglePlayback"
