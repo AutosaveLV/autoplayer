d125e2b63f3e8be7554a3fd72fdd2a096fd9dccd
Filips Alpe <filips.alpe@gmail.com>
2023-12-14 11:31:48 +0200

    Update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,33 @@ export default defineNuxtConfig({
     "@unocss/reset/tailwind-compat.css",
     "primevue/resources/themes/lara-dark-purple/theme.css",
   ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.png", color: "#ba5bd5" },
+      ],
+      meta: [
+        { name: "msapplication-TileColor", content: "#c0c0c0" },
+        { name: "theme-color", content: "#191919" },
+      ],
+    },
+  },
 });
